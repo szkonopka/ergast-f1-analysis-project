de12273c5c5b0584ef031ac21d472fbab55ce24a
cloudera <cloudera@quickstart.cloudera>
2019-11-24 12:11:44 -0800

    Add bash for creating a dir structure of project.

---
+++ create-dirs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Current HDFS dir structure"
+hdfs dfs -ls
+
+hdfs dfs -mkdir Final_data Data_source New_batch_data New_incremental_data New_incremental_data/Results New_incremental_data/Lap_times Logs
+
+echo "HDFS dir structure after structure creation"
+hdfs dfs -ls
