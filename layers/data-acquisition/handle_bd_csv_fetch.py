2dc9eccc704bb787642349eda981a438b8b735a4
Szymon Konopka <samzonee@gmail.com>
2019-12-09 00:07:22 +0100

    CSV files validation.

--- layers/data-acquisition/handle_bd_csv_fetch.py
+++ layers/data-acquisition/handle_bd_csv_fetch.py
@@ -4,19 +4,32 @@ import sys
 import os
 import prepare_logger
 import posixpath as path
-import re
-
-def validate_csv(file):
-	pass
-	
-def move_csv_to_input_files(file):
-	pass
+import pandas as pd
 
 logger = prepare_logger.get_configured('ergast_handle_api', 'ergast_handle_api.log')
 
 ENTRY_CSV_DIR = '/home/ergast/entry'
 TRASH_CSV_DIR = '/home/ergast/trash'
+INPUT_FILES_DIR = '/home/ergast/input_files'
 CSV_EXT = '.csv'
+
+EXPECTED_FILES_COL_LENGTH = {
+    'circuits': 9,
+    'constructors': 5,
+    'driver': 5,
+    'lap_times': 6,
+    'races': 8,
+    'results': 18,
+    'status': 2
+}
+
+def validate_csv(file):
+	df = pd.read_csv(path.join(ENTRY_CSV_DIR, file), header=None)
+	return len(df.columns) == EXPECTED_FILES_COL_LENGTH[file[:-4]]
+	
+def move_csv_to_input_files(file):
+	logger.debug("Send file {} to {}".format(file, INPUT_FILES_DIR))
+	os.replace(path.join(ENTRY_CSV_DIR, file), path.join(INPUT_FILES_DIR, file))
 
 logger.debug("Step 1 - load all files")
 files = (file for file in os.listdir(ENTRY_CSV_DIR) if path.isfile(path.join(ENTRY_CSV_DIR, file)))
@@ -32,6 +45,7 @@ for file in files:
 		csv_files.append(file)
 
 logger.debug("Step 3 - validate all fetched csv files")
-for file in files:
+for file in csv_files:
 	if (validate_csv(file)):
+		logger.debug("File {} has proper column size".format(file))
 		move_csv_to_input_files(file)
