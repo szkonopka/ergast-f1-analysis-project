2beaff10c72707cb8f8eeb67bea7bd56afe81a7e
t-zilla <dunajpascal@gmail.com>
2020-01-08 12:56:58 -0800

    CSV apostrophes

--- layers/data-processing/load-csv-files-to-hive.sql
+++ layers/data-processing/load-csv-files-to-hive.sql
@@ -4,8 +4,11 @@ set hive.execution.engine=mr;
 
 CREATE EXTERNAL TABLE IF NOT EXISTS ergast_results.circuits_csv
 (circuitId varchar(50), name varchar(100))
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+   "separatorChar" = ",",
+   "quoteChar"     = "\""
+)
 STORED AS TEXTFILE;
 
 LOAD DATA INPATH '/user/cloudera/Data_source/circuits.csv' INTO TABLE ergast_results.circuits_csv;
@@ -15,8 +18,11 @@ INSERT INTO TABLE ergast_results.circuits SELECT * FROM ergast_results.circuits_
 
 CREATE EXTERNAL TABLE IF NOT EXISTS ergast_results.races_csv
 (raceId int, year int, circuitId varchar(50))
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+   "separatorChar" = ",",
+   "quoteChar"     = "\""
+)
 STORED AS TEXTFILE;
 
 LOAD DATA INPATH '/user/cloudera/Data_source/races.csv' INTO TABLE ergast_results.races_csv;
@@ -26,8 +32,11 @@ INSERT INTO TABLE ergast_results.races SELECT * FROM ergast_results.races_csv;
 
 CREATE EXTERNAL TABLE IF NOT EXISTS ergast_results.lapTimes_csv
 (raceId int, lap int, driverId varchar(50), position int)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+   "separatorChar" = ",",
+   "quoteChar"     = "\""
+)
 STORED AS TEXTFILE;
 
 LOAD DATA INPATH '/user/cloudera/Data_source/lap_times.csv' INTO TABLE ergast_results.lapTimes_csv;
@@ -49,8 +58,11 @@ INSERT INTO TABLE ergast_results.drivers SELECT * FROM ergast_results.drivers_cs
 CREATE EXTERNAL TABLE IF NOT EXISTS ergast_results.results_csv
 (resultId int, raceId int, grid int, positionOrder int, statusId int, 
  points int, fastestLap float, fastestLapSpeed float, laps int, time float, driverId varchar(50), constructorId varchar(50))
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+   "separatorChar" = ",",
+   "quoteChar"     = "\""
+)
 STORED AS TEXTFILE;
 
 LOAD DATA INPATH '/user/cloudera/Data_source/results.csv' INTO TABLE ergast_results.results_csv;
@@ -60,8 +72,11 @@ INSERT INTO TABLE ergast_results.results SELECT * FROM ergast_results.results_cs
 
 CREATE EXTERNAL TABLE IF NOT EXISTS ergast_results.status_csv 
 (statusId int, status varchar(100))
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+   "separatorChar" = ",",
+   "quoteChar"     = "\""
+)
 STORED AS TEXTFILE;
 
 LOAD DATA INPATH '/user/cloudera/Data_source/status.csv' INTO TABLE ergast_results.status_csv;
@@ -71,8 +86,11 @@ INSERT INTO TABLE ergast_results.status SELECT * FROM ergast_results.status_csv;
 
 CREATE EXTERNAL TABLE IF NOT EXISTS ergast_results.constructors 
 (constructorId varchar(50), name varchar(50), nationality varchar(20))
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+   "separatorChar" = ",",
+   "quoteChar"     = "\""
+)
 STORED AS TEXTFILE;
 
 LOAD DATA INPATH '/user/cloudera/Data_source/constructors.csv' INTO TABLE ergast_results.constructors_csv;
